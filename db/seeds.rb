1e685c21c96a879732db73b4c74108b15a5ed66b
minyoung <minyoung9353@gmail.com>
2013-12-19 15:58:57 +0900

    fas

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 0.upto(10) do |x|
 Company.create(title: '누야하우스', subtitle: '누야하우스입니다.', content:'<h3 style="color:#e3742d;">자연과 함께하는 누야하우스</h3><img src="/images/image_01.png"><h3>회사소개</h3><p>장애인들이 사회 구성원으로서 자립하고 성장할 수 있는 일터를 목표로 하며,</p>
-      <p>모든 제품은 (주)닥터메이드와 공동개발을 통해 최고의 천연제품을 제공하기 위해 노력하고 있습니다.</p>', thumbnail_image: 'nuya_thumbnail.gif', big_image: 'nuyamain.jpg', homepage: 'iinme.co.kr', shopping: 'iinme.co.kr' )
+      <p>모든 제품은 (주)닥터메이드와 공동개발을 통해 최고의 천연제품을 제공하기 위해 노력하고 있습니다.</p>', thumbnail_image: 'nuya_thumbnail.gif', big_image: 'nuya_main.png', homepage: 'iinme.co.kr', shopping: 'iinme.co.kr' )
 
 end
 
