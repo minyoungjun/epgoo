20383464d9988f21cb69f4f5f5e77c80aad9da69
minyoung <minyoung9353@gmail.com>
2013-12-21 16:58:48 +0900

    af

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,5 +1,22 @@
 class AdminController < ApplicationController
 
+  def change_photo
+      photo = Photo.find(params[:id])
+      photo.link_url = params[:link_url]
+    if params[:file] != nil
+      photo.file_name = SecureRandom.hex(5) + params[:file].original_filename[-5..-1]
+      f = File.open(Rails.root.join("public", "images", "gallery", photo.file_name), "wb")
+      f.write(params[:file].read)
+      f.close
+    end
+
+    photo.save
+   
+
+    redirect_to :back
+
+  end
+
   def new_info
     company = Company.new
     company.title = params[:title]
@@ -9,7 +26,26 @@ class AdminController < ApplicationController
     company.phone = params[:phone]
     company.homepage = params[:homepage]
     company.shopping = params[:shopping]
-    company.save
+    company.big_image = SecureRandom.hex(5) + params[:big_image].original_filename[-5..-1]
+    company.thumbnail_image = SecureRandom.hex(5) + params[:thumbnail_image].original_filename[-5..-1]
+    if params[:portfolio] != nil
+      company.portfolio = SecureRandom.hex(5) + params[:portfolio].original_filename[-5..-1]
+    end
+
+    if company.save
+      big_image = File.open(Rails.root.join("public", "images", "company", company.big_image), "wb")
+      big_image.write(params[:big_image].read)
+      big_image.close
+      thumbnail_image = File.open(Rails.root.join("public", "images", "company", company.thumbnail_image), "wb")
+      thumbnail_image.write(params[:thumbnail_image].read)
+      thumbnail_image.close
+      if params[:portfolio] != nil
+        portfolio = File.open(Rails.root.join("public", "portfolio", company.portfolio), "wb")
+        portfolio.write(params[:portfolio].read)
+        portfolio.close
+      end
+    end
+
     redirect_to :action => "edit",
                 :id => company.id
   end
